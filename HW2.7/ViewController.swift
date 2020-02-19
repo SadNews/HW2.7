37de3b70066346d687f62ad9326a12d7271fe48e
Andrey Ushakov <sadnews@ya.ru>
2020-02-20 00:26:12 +0300

    Initial Commit

---
+++ HW2.7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.7
+//
+//  Created by Admin on 20.02.2020.
+//  Copyright © 2020 UshakovAndrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
